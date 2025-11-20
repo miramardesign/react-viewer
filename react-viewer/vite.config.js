33360f6d4d3ef19838dab83f1a566f111cc806ec
mhazzard <miramardesign@gmail.com>
2025-11-20 20:38:49 -0300

    fix config?

--- react-viewer/vite.config.js
+++ react-viewer/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/react-viewer/',
     plugins: [
         react(),
         VitePWA({
